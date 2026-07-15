524b5a342071231d32ad745a535c9da1e6296555
viralbhuptani12 <viralbhuptani12@gmail.com>
2026-07-15 16:30:47 +0530

    Add year-aware current-month summary view

--- tracker.py
+++ tracker.py
@@ -39,15 +39,44 @@ def view_summary():
 
     print()
 
+def view_month():
+    total = 0.0
+    by_category = {}
+
+    with open("expenses.txt", "r") as f:
+        for line in f:
+            parts =(line.strip()).split(" | ")
+            tx_date = parts[0].split("-")
+            if datetime.now().strftime("%Y-%m") == tx_date[0]+"-"+tx_date[1]:
+                category = parts[1].strip()
+                amount = float(parts[2])
+                total = total + amount
+                if category in by_category:
+                    by_category[category] = by_category[category] + amount
+                else:
+                    by_category[category] = amount
+
+        
+    print(f"\n Total spent = ₹{total:.2f} \n")
+    print("Where did you spend: ")
+    ##print(by_category)
+    sorted_categories = sorted(by_category.items(), key=lambda item:item[1], reverse = True)
+    for name,amount in sorted_categories:
+        print(f"{name}: ₹{amount:.2f}")
+
+    print()
+
 while True:
-    user_choice = input( "What do you want to do add[a] or view summary[v] or quit[q]?\n").lower()
+    user_choice = input( "What do you want to do add[a] or view summary[v] or view current month summary[c] or quit[q]?\n").lower()
     if user_choice == "a":
         add_expense()
     elif user_choice == "v":
         view_summary()
+    elif user_choice == "c":
+        view_month()
     elif user_choice == "q":
         break
-    else:
+    else: 
         print("Please enter a correct choice")
 
 
